8f0d859ee42f313bb837d07495b332e0c9383199
sunadoi <y.sunadoi@gmail.com>
2020-04-30 16:37:49 +0900

    create addperson deleteperson

--- src/store/reducers/person.js
+++ src/store/reducers/person.js
@@ -1,9 +1,27 @@
+import * as actionType from '../actions';
+
 const initialState = {
-  name: 'Max',
-  age: 0
+  persons: []
 }
 
 const person = (state = initialState, action) => {
+  switch (action.type) {
+    case actionType.ADDPERSON:
+      const newPerson = {
+        id: Math.random(), // not really unique but good enough here!
+        name: 'Max',
+        age: Math.floor( Math.random() * 40 )
+      }
+      return {
+        ...state,
+        persons: state.persons.concat(newPerson)
+      }
+    case actionType.DELETEPERSON:
+      return {
+        ...state,
+        persons: state.persons.filter(person => person.id !== action.personId)
+      }
+  }
   return state;
 }
 
